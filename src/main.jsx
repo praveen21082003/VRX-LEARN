a950af51674af11c4a03613459932f7abc545885
Praveen <yenugulapraveenkumar@gmail.com>
2025-11-19 10:27:50 +0530

    profile solved

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ContextProvider } from "./components/context/ContextProvider.jsx";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 import "./index.css";
 import App from "./App.jsx";
@@ -9,6 +10,7 @@ import App from "./App.jsx";
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <ContextProvider>
+      <SpeedInsights />
       {/* <StrictMode> */}
       <App />
       {/* </StrictMode> */}
